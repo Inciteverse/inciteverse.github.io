524bdb339acca738b8f4bbf1c3395c8f74d18c94
ivfoundershan <shaan@inciteverse.com>
2022-05-11 12:22:45 +0530

    Changes done!

--- static/js/script.js
+++ static/js/script.js
@@ -5,18 +5,27 @@ themeSwitch.addEventListener("change", function() {
     document.querySelector(".light").classList.add("dark");
     document
       .querySelector(".brand img[src]")
-      .setAttribute("src", "../static/images/dark-inciteverse.png");
+      .setAttribute("src", "./static/images/dark-inciteverse.png");
     document
       .querySelector("#hero")
-      .setAttribute("src", "../static/images/dark-icon.png");
+      .setAttribute("src", "./static/images/dark-icon.png");
   } else {
     document.body.classList.remove("dark");
     document.querySelector(".dark").classList.remove("dark");
     document
       .querySelector(".brand img[src]")
-      .setAttribute("src", "../static/images/inciteverse.png");
-      document
+      .setAttribute("src", "./static/images/inciteverse.png");
+    document
       .querySelector("#hero")
-      .setAttribute("src", "../static/images/icon.png");
+      .setAttribute("src", "./static/images/icon.png");
   }
+});
+
+var typed = new Typed(".animate-terms", {
+  strings: ["Customer satisfaction.", "Creative ides.", "Business needs."],
+  typeSpeed: 60,
+  backSpeed: 60,
+  loop: true,
+  loopCount: Infinity,
+  startDelay: 500
 });
