18137e0fd9c189b4cb933b28e5860e76fe162ebf
ivfoundershan <shaan@inciteverse.com>
2022-06-09 19:14:28 +0530

    Upgraded version

--- js/main.js
+++ js/main.js
@@ -1,94 +1,116 @@
-(function ($) {
-    "use strict";
+(function($) {
+  "use strict";
 
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
-    
-    
-    // Initiate the wowjs
-    new WOW().init();
+  // Spinner
+  var spinner = function() {
+    setTimeout(function() {
+      if ($("#spinner").length > 0) {
+        $("#spinner").removeClass("show");
+      }
+    }, 1);
+  };
+  spinner();
 
+  // Initiate the wowjs
+  new WOW().init();
 
-    // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 45) {
-            $('.navbar').addClass('sticky-top shadow-sm');
-        } else {
-            $('.navbar').removeClass('sticky-top shadow-sm');
-        }
-    });
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
+  // Sticky Navbar
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 45) {
+      $(".navbar").addClass("sticky-top shadow-sm");
+    } else {
+      $(".navbar").removeClass("sticky-top shadow-sm");
+    }
+  });
 
+  // Back to top button
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function() {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
 
-    // Skills
-    $('.skill').waypoint(function () {
-        $('.progress .progress-bar').each(function () {
-            $(this).css("width", $(this).attr("aria-valuenow") + '%');
-        });
-    }, {offset: '80%'});
+  // Skills
+  $(".skill").waypoint(
+    function() {
+      $(".progress .progress-bar").each(function() {
+        $(this).css("width", $(this).attr("aria-valuenow") + "%");
+      });
+    },
+    { offset: "80%" }
+  );
 
+  // Facts counter
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 2000
+  });
 
-    // Facts counter
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 10,
-        time: 2000
-    });
-
-
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        margin: 25,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="bi bi-chevron-left"></i>',
-            '<i class="bi bi-chevron-right"></i>'
-        ],
-        responsive: {
-            0:{
-                items:1
-            },
-            992:{
-                items:2
-            }
-        }
-    });
-
-
-    // Portfolio isotope and filter
-    var portfolioIsotope = $('.portfolio-container').isotope({
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
-    });
-    $('#portfolio-flters li').on('click', function () {
-        $("#portfolio-flters li").removeClass('active');
-        $(this).addClass('active');
-
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
-    });
-    
+  // Testimonials carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    margin: 25,
+    dots: false,
+    loop: true,
+    nav: true,
+    navText: [
+      '<i class="bi bi-chevron-left"></i>',
+      '<i class="bi bi-chevron-right"></i>'
+    ],
+    responsive: {
+      0: {
+        items: 1
+      },
+      992: {
+        items: 2
+      }
+    }
+  });
 })(jQuery);
 
+const CreateLogoCard = logos => {
+  const workDiv = document.getElementById("work");
+  let html = "";
+  logos.forEach(logo => {
+    html += `<div class="col-lg-4 col-md-6 portfolio-item first wow fadeInUp" data-wow-delay="0.5s">
+    <div class="rounded overflow-hidden">
+        <div class="position-relative overflow-hidden">
+            <img class="img-fluid w-100" style="width:350px; height:350px"  src="${logo}" alt="">
+            <div class="portfolio-overlay">
+                <a class="btn btn-square btn-outline-light mx-1" href="${logo}" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+            </div>
+        </div>
+        <div class="bg-light p-4">
+            <p class="text-primary fw-medium mb-2">Logo Design</p>
+            <h5 class="lh-base mb-0">Digital Agency Website Design And Development</a>
+        </div>
+    </div>
+</div>`;
+  });
+  workDiv.insertAdjacentHTML("afterbegin", html);
+};
+
+const fetchLogos = () => {
+  const Logos = [];
+  // "https://firebasestorage.googleapis.com/v0/b/inciteverse.appspot.com/o/Logos%2F" +
+  //i +
+  //  ".png?alt=media&token=c3796d23-f921-4a6f-8107-84dc8b0638a6://inciteverse.appspot.com/Logos";
+  for (let i = 1; i <= 3; i++) {
+    Logos.push(
+      "https://firebasestorage.googleapis.com/v0/b/inciteverse.appspot.com/o/Logos%2F" +
+        i +
+        ".png?alt=media&token=c3796d23-f921-4a6f-8107-84dc8b0638a6://inciteverse.appspot.com/Logos"
+    );
+  }
+  CreateLogoCard(Logos);
+  return Logos;
+};
+
+fetchLogos();
